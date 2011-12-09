e28590a296d9695897b0d4718f9a3592cf6a2d5d
Indu R Nair <induvallikunnath@gmail.com>
2011-12-09 21:00:24 +0530

    some errors fixed

--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -2,11 +2,10 @@ module GalleriesHelper
 
   def tag_list(galleries)
     @tag_list = ""
-    galleries.each do |tag|
-      @tag_list =  @tag_list + link_to(tag.tags,:controller=>'galleries',:action=>'index',:tag_id=>tag.tags) + ", "
-    end
-    @tag_list = @tag_list.chop![0..-2] if @tag_list
-
+    galleries.each do |gallery|
+        @tag_list =  @tag_list + link_to(gallery.tags,:controller=>'galleries',:action=>'index',:tag_id=>gallery.tags) + ", "
+      end
+    @tag_list = @tag_list.chop![0..-2]
   end
 
 end
