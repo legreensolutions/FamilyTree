c137ae9405a4c4683b9431c3be63c1cf1df69e54
Indu R Nair <induvallikunnath@gmail.com>
2012-01-02 19:06:06 +0530

    display of repeated tags remvoed from gallery and news items index pages

--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -2,12 +2,16 @@ module GalleriesHelper
 
 def tag_list(galleries)
     @tag_list = ""
-
+    prev_tag = ""
     galleries.each do |gallery|
       unless gallery.tags.blank?
-        @tag_list =  @tag_list + link_to(gallery.tags,:controller=>'galleries',:action=>'index',:tag_id=>gallery.tags) + ", "
+        if prev_tag.to_s != gallery.tags.to_s
+          @tag_list =  @tag_list + link_to(gallery.tags,:controller=>'galleries',:action=>'index',:tag_id=>gallery.tags) + ", "
+
+        end
+        prev_tag = gallery.tags
       end
-      end
+    end
 
     @tag_list = @tag_list.chop![0..-2] if !@tag_list.empty?
   end
