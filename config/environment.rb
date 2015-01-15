13291ab7ab7f878ce536a122b85e720f9a55bc97
Sujith <sujithsurendran@yahoo.com>
2015-01-15 09:51:16 +0530

    removed sendgrid...2

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Familynew::Application.initialize!
 
-#ActionMailer::Base.smtp_settings = {
-#  :user_name => 'sujithsurendran',
-#  :password => 'SendGridSuj!th123',
-#  :domain => 'mannekkatil.com',
-#  :address => 'smtp.sendgrid.net',
-#  :port => 587,
-#  :authentication => :plain,
-#  :enable_starttls_auto => true
-#}
 
