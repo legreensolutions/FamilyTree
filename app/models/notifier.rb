211bf3399d5b7bbaa3a22694a3e3ebe6a173cbed
sujithsurendran@yahoo.com <sujithsurendran@yahoo.com>
2015-06-08 14:32:16 +0530

    syntax error corrected

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
 
   def password_reset_instructions(user)
  			subject       "Password Reset Instructions"
- 			from          "mail@mannekattil.com""mail@mannekattil.com"
+ 			from          "mail@mannekattil.com"
  			recipients    user.email
  			sent_on       Time.now
  			body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
