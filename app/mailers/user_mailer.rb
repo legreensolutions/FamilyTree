0aa163ee41bcfdebedfcb55834c3928f0994bb07
sujithsurendran@yahoo.com <sujithsurendran@yahoo.com>
2015-06-15 21:56:51 +0530

    app/mailers/user_mailer.rb syntax err

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ActionMailer::Base
 
 
       @user = user
-      @name = @user.member.name
+      @name = user.member.name
       
       #mail :to => user.email, :subject => "Password Reset Instructions from mannekattil.com", :from => "mannekattil@mannekattil.com", :body => :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
 
