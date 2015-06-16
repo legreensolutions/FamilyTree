0e7875586e7e54c9a6aa05de43588d475cfe6f53
sujithsurendran@yahoo.com <sujithsurendran@yahoo.com>
2015-06-16 21:54:30 +0530

    reconfigured for send mail instead of gmail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,11 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "mannekattil.com",
-  :user_name            => "suthsuren",
-  :password             => "",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+# now using sendmail instead of gmail
+
+#ActionMailer::Base.smtp_settings = {
+#  :address              => "smtp.gmail.com",
+#  :port                 => 587,
+#  :domain               => "mannekattil.com",
+#  :user_name            => "suthsuren",
+#  :password             => "",
+#  :authentication       => "plain",
+#  :enable_starttls_auto => true
+#}
